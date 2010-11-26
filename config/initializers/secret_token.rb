58ed920656dee358e5cbf4a93a5fc3ae509ccfe6
Jussi <jui@Descom-pre-9.local>
2010-11-27 00:03:55 +0200

    rake rails:update

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Ryweb::Application.config.secret_token = '9d5076de8adcf13e9b49f9defe20bf5ba0822d5400864cf90ae0118a8b2acc1eb8c7be26101da4cc30c5f8b422d5c61ff5513c1b9bf364072c62fc1defea8400'
+Ryweb::Application.config.secret_token = '689b0042125cbbc7ca215f19eb648308909c1616c9ccc3c40ba73a1aeb732f1c80533da9968638ec3bc9c07313f70452b1738c398785371fa9c68c3066a5de83'
