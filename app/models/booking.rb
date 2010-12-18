bdef325f9e2e681006833cbdb5b6d2b34223e9a7
Daniel Linjama <daniel@linjamaTM-ubuntu.(none)>
2010-12-18 17:24:46 +0200

    Validoinnit kuntoon ja varaamaton? vaihdettu validointeihin, jolloin toimii myös muokatessa

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,19 +2,40 @@ class Booking < CustomerData
   has_and_belongs_to_many :rooms
   validates_presence_of   :start_time
   validates_presence_of   :end_time
+  validates_presence_of   :reserver
+  validate :varaamaton
+  validate :keston_pitaa_olla_positiivinen
 
-	def varaamaton?		
-		for room in self.rooms
-			for b in room.bookings
-				if(self.id != b.id)
-					unless(self.start_time >= b.end_time || self.end_time <= b.start_time)
-						return false
-					end
-				end
-			end
-		end		
+	def varaamaton?	
+	  for room in self.rooms
+	    for b in room.bookings
+	      if(self.id != b.id)
+	        unless(self.start_time >= b.end_time || self.end_time <= b.start_time)
+		  return false
+		end
+              end
+	    end
+	  end		
 	
-		return true
+	  return true
+	end
+	
+	def varaamaton	
+	  for room in self.rooms
+	    for b in room.bookings
+	      if(self.id != b.id)
+	        unless(self.start_time >= b.end_time || self.end_time <= b.start_time)
+		  errors.add(:tila, 'on jo varattu')
+		end
+              end
+	    end
+	  end
+	end
+	
+	def keston_pitaa_olla_positiivinen
+		if end_time.to_i < start_time.to_i
+			errors.add(:end_time, 'pitää olla alkamisajan jälkeen')
+		end
 	end
 	
 	# Returns abbreviated weekday number (Sunday = 0) when time is given
